7b0d7633cbe70e54693863af91e726a453005686
HB <52573059+FreshestPrince@users.noreply.github.com>
2020-02-09 21:40:51 +0000

    Comments added, made into one function. Must implement week long only. Too many loops. Must implement lunch.  Must create better UI.

--- Test.py
+++ Test.py
@@ -1,13 +1,41 @@
 from datetime import datetime
 from xlwt import Workbook
-def ExcelTime(Array, DayLimit, TimeLimitA, TimeLimitB, Name):
+def Timetable():
+    SubjectsNo = int(input("Please enter the number of subjects: "))
+    DayLimit = 6 # For weekdays
+    AllSubjects = dict(input("Please insert subjects followed by their number of hours \n").split()
+                       for _ in range(SubjectsNo)) # Create a dict of subject followed by the number of hours
+    AllSubjects = {str(k): int(v) for k, v in AllSubjects.items()}  # Convert hours from strings to ints
+    OnlySubs = list(AllSubjects.keys())  # Getting list of just subjects
+    TimeLimitA = int(input("Please input the opening time in 24 hrs: ")) + 1  # Starting time for the day
+    TimeLimitB = int(input("Please input the closing time in 24 hrs: ")) + 2  # Closing time for the day
+    Name = input("What would you like to name the Excel file: ")
+
+
+    # Below makes an array for the subjects in order
+    day = 0
+    Number = 0
+    Array = []
+    while day < DayLimit:
+        time = TimeLimitA
+        while SubjectsNo > 0:
+            while AllSubjects[OnlySubs[Number]] > 0:
+                Array.append(OnlySubs[Number])
+                AllSubjects[OnlySubs[Number]] -= 1
+                time += 1
+            Number += 1
+            SubjectsNo -= 1
+        day += 1
+
     DayDict = {1: "Monday", 2: "Tuesday", 3: "Wednesday", 4: "Thursday", 5: "Friday", 6: "Saturday", 7: "Sunday"}
     wb = Workbook()
     sheet1 = wb.add_sheet('Timetable 1')
     sheet1.write(0,0, "Time")
     NewTimeLimitA = TimeLimitA
     x = 1
-    while NewTimeLimitA < TimeLimitB:
+
+    #  Below makes the first column for the time
+    while NewTimeLimitA < TimeLimitB-1:
         d = datetime.strptime(str(NewTimeLimitA) + ":00", "%H:%M")
         d = d.strftime("%I:%M %p")
         sheet1.write(x, 0, d)
@@ -19,6 +47,8 @@ def ExcelTime(Array, DayLimit, TimeLimitA, TimeLimitB, Name):
         x += 1
     Number = 1
     day = 1
+
+    #  Writing subjects into excel sheets into times
     while day < DayLimit:
         time = TimeLimitA+1
         while time < TimeLimitB:
@@ -35,30 +65,5 @@ def ExcelTime(Array, DayLimit, TimeLimitA, TimeLimitB, Name):
                 time += 1
         day += 1
 
-    wb.save(Name + ".xls")
-def GetArray():
-
-    SubjectsNo = int(input("Please enter the number of subjects: "))
-    DayLimit = 5
-    AllSubjects = dict(input("Please insert subjects followed by their number of hours \n").split() for _ in range(SubjectsNo))
-    AllSubjects = {str(k): int(v) for k, v in AllSubjects.items()}  # Convert hours from strings to ints
-    OnlySubs = list(AllSubjects.keys())  # Getting list of just subjects
-    TimeLimitA = int(input("Please input the opening time in 24 hrs: ")) + 1
-    TimeLimitB = int(input("Please input the closing time in 24 hrs: ")) + 1
-    Name = input("What would you like to name the Excel file: ")
-    day = 0
-    Number = 0
-    Array = []
-    while day < DayLimit:
-        time = TimeLimitA
-        while SubjectsNo > 0:
-            while AllSubjects[OnlySubs[Number]] > 0:
-                Array.append(OnlySubs[Number])
-                AllSubjects[OnlySubs[Number]] -= 1
-                time += 1
-            Number += 1
-            SubjectsNo -= 1
-        day += 1
-    ExcelTime(Array, DayLimit, TimeLimitA, TimeLimitB, Name)
-
-GetArray()
+    wb.save(Name + ".xls") # Save file as an excel sheet
+Timetable()
